46c87dd9b228f50280c012aabfbe153ac85f23ed
mainz1011 <mainz1011@hotmail.com>
2023-06-13 23:35:40 +0300

    Add files via upload

---
+++ relationship 1_scatter plot.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import csv
+  
+x = ["Classic_bike; Member", "Classic_bike; Casual", "Electric_bike; Member", "Electric_bike; Casual", "Docked_bike; Member", "Docked_bike; Casual"]
+y = [0, 0, 0, 0, 0, 0]
+
+with open('JC-202112-citibike-tripdata.csv') as csvfile:
+    plots = csv.reader(csvfile, delimiter = ',')
+
+    for row in plots:
+        if row[1] == "classic_bike" and row[12] == "member":
+            y[0] += 1
+        elif row[1] == "classic_bike" and row[12] == "casual":
+            y[1] += 1
+        elif row[1] == "electric_bike" and row[12] == "member":
+            y[2] += 1
+        elif row[1] == "electric_bike" and row[12] == "casual":
+            y[3] += 1
+        elif row[1] == "docked_bike" and row[12] == "member":
+            y[4] += 1
+        elif row[1] == "docked_bike" and row[12] == "casual":
+            y[5] += 1
+
+plt.scatter(x, y, color = 'b', s = 50)
+plt.ylabel("Number of rides")
+plt.title("The relationship between the “Rideable type” and the “Member or casual” category", fontsize = 20)
+plt.show()
