7fa75daaba23a7de6480907cb0c4544aed01b6e0
mainz1011 <mainz1011@hotmail.com>
2023-06-14 22:37:17 +0300

    Add files via upload

---
+++ relationship 2_scatter plot.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
+import csv
+  
+x = []
+y = [0] * 31
+
+for days in range(31):
+    x.append(days + 1)
+
+with open('JC-202112-citibike-tripdata.csv') as csvfile:
+    plots = csv.reader(csvfile, delimiter = ',')
+
+    for row in plots:
+        timeStr = row[2]
+        timeArr = timeStr.split("/")
+        if len(timeArr) == 3:
+            day = int(timeArr[1])
+            y[day - 1] += 1
+
+plt.scatter(x, y, color = 'purple', s = 50)
+plt.xlabel("Day of December 2021")
+plt.ylabel("Number of rides")
+plt.xlim([1,31])
+plt.xticks(rotation = 25)
+plt.title("Number of bike rides in each day of December 2021", fontsize = 20)
+plt.show()
